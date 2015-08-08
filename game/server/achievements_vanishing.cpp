ae602e586955d458b480c5c507da34d30a929493
George Wu <micstu@qq.com>
2015-08-09 00:30:41 +0800

    Added some instructions for adding achievements.

--- game/server/achievements_vanishing.cpp
+++ game/server/achievements_vanishing.cpp
@@ -13,6 +13,28 @@
 
 #define ACHIEVEMENT_VANISHING_PRESSED_TEST_BUTTON (201)
 
+// Achievement settings guide:
+//
+// Step 1: Prepare Achievements List
+// Step 2: Code Your Achievements
+//       - Suppose that the achievement name is MOD_ACHIEVEMENT.
+//       - * achievementName in macro DECLARE_ACHIEVEMENT => "MOD_ACHIEVEMENT"
+//       - * achievement name provided for Hammer (in .fgd) => ACHIEVEMENT_EVENT_MOD_ACHIEVEMENT (see logic_achievement.cpp)
+//       - * global achievement name => MOD_ACHIEVEMENT
+//       - * achievement icon file name => MOD_ACHIEVEMENT.vmt
+//       - Code the class, derived from CBaseAchievement.
+//       - The achievement class should contains flags either ACH_SAVE_WITH_GAME or ACH_SAVE_GLOBAL. Lacking this leads to an assertion failure.
+//       - If you want to award the achievement without Steamworks (needs Internet connection and Steamworks authorization), override the achievements class' AlwaysEnabled() method. (See CAchievementMgr::AwardAchievement())
+// Step 3: Set Up the Icon
+//       - 1. Prepare a SQUARE image.
+//       - 2. Use VTFEdit to convert it into .vtf format, using default parameters.
+//       - 3. Generate .vmt file using the parameter: Shader = Decal, Additive = true, Translucent = false.
+//       - 4. Save .vtf and .vmt to {path_to_your_mod}/materials/vgui/achievements/MOD_ACHIEVEMENT[.vmt/.vtf].
+// Step 4: Activate the Achievement
+//       - Open hammer and insert a logic_achievement point entity naming "achiever", with AchievementName property set to "ACHIEVEMENT_EVENT_MOD_ACHIEVEMENT" (or equivalent selection value, based on custom FGD).
+//       - Add an output, setting its target to achiever's FireEvent() input.
+// Step 5: Run the Map and Enjoy
+
 class CAchievementVanishingPressedTestButton : public CBaseAchievement
 {
 
